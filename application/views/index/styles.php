8c25f16bb1fc0127005ae698bae536e96cf0767d
Ezequiel Paolillo <ezequiel.paolillo@univtec.com>
2022-10-18 17:56:03 -0300

    Added stilos nuevos

--- application/views/index/styles.php
+++ application/views/index/styles.php
@@ -2,6 +2,22 @@
     .form-control{
         width: inherit;
     }
+    .filter-row{
+        display: table-row;
 
+    }
+    .table{
+        overflow-x: scroll;
+        display: inline-block;
+    }
+
+    .grocery-crud-table .btn-group.open ul{
+        display: block;
+        padding: 10px;
+    }
+
+    .grocery-crud-table .btn-group.open ul a{
+        color: #000;
+    }
 
 </style>
