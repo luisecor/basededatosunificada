893bc25d4a6abf6ba58faf54532fbd7ca4236045
Luis Ezequiel Cordoba <luis_ecor@hotmail.com>
2023-10-11 13:16:35 -0300

    edith.php

--- assets/grocery_crud/themes/bootstrap/views/edit.php
+++ assets/grocery_crud/themes/bootstrap/views/edit.php
@@ -61,33 +61,33 @@ include(__DIR__ . '/common_javascript_vars.php');
                                 <div id='report-error' class='report-div error bg-danger' style="display:none"></div>
                                 <div id='report-success' class='report-div success bg-success' style="display:none">
                                 <script>
-                                   // Select the node that will be observed for mutations
-                                    const targetNode = document.querySelector('#report-success');
+                                //    // Select the node that will be observed for mutations
+                                //     const targetNode = document.querySelector('#report-success');
 
 
-                                    // Options for the observer (which mutations to observe)
-                                    const config = { attributes: true, childList: true, subtree: true };
+                                //     // Options for the observer (which mutations to observe)
+                                //     const config = { attributes: true, childList: true, subtree: true };
 
-                                    // Callback function to execute when mutations are observed
-                                    const callback = (mutationList, observer) => {
-                                    for (const mutation of mutationList) {
-                                        if (mutation.type === "childList") {
-                                        console.log("A child node has been added or removed.");
-                                        history.back();
-                                        } else if (mutation.type === "attributes") {
-                                        console.log(`The ${mutation.attributeName} attribute was modified.`);
-                                        }
-                                    }
-                                    };
+                                //     // Callback function to execute when mutations are observed
+                                //     const callback = (mutationList, observer) => {
+                                //     for (const mutation of mutationList) {
+                                //         if (mutation.type === "childList") {
+                                //         console.log("A child node has been added or removed.");
+                                //         history.back();
+                                //         } else if (mutation.type === "attributes") {
+                                //         console.log(`The ${mutation.attributeName} attribute was modified.`);
+                                //         }
+                                //     }
+                                //     };
 
-                                    document.addEventListener("DOMContentLoaded", function(event) {
-                                         // Start observing the target node for configured mutations
-                                    observer.observe(targetNode, config);
+                                //     document.addEventListener("DOMContentLoaded", function(event) {
+                                //          // Start observing the target node for configured mutations
+                                //     observer.observe(targetNode, config);
                                     
-                                    })
+                                //     })
 
-                                    // Create an observer instance linked to the callback function
-                                    const observer = new MutationObserver(callback);
+                                //     // Create an observer instance linked to the callback function
+                                //     const observer = new MutationObserver(callback);
 
                                    
                                 </script>
